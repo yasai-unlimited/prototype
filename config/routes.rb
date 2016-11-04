abe542e5ba6ffe3864aa216c06d4ea0596148c86
kei-tamiya <webcreatorhouse@gmail.com>
2016-11-04 21:29:22 +0900

    [add] add open_id, name, sex column to users, and implement open_id to controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'static_pages#home'
+  devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
