db385b5814a45dc649770e9afaf9af83380ae101
Asami DOI <doiasami1219@gmail.com>
2016-11-09 12:23:59 +0900

    [add] album model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109021726) do
+ActiveRecord::Schema.define(version: 20161109032245) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "families", force: :cascade do |t|
     t.string   "name"
