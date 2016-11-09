8c0aad61d85d3f932ae63fdb53c7c9ef6653719f
Asami DOI <doiasami1219@gmail.com>
2016-11-09 12:44:48 +0900

    [modify] album picture model 関連づけ

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
 class Picture < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  belongs_to :album
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109032245) do
+ActiveRecord::Schema.define(version: 20161109032433) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20161109032245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.integer  "album_id"
   end
 
   create_table "users", force: :cascade do |t|
