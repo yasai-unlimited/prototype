672950d7600f9fb86f78c057470feafc33ad6602
kei-tamiya <webcreatorhouse@gmail.com>
2016-11-09 17:32:09 +0900

    [modify] family_post削除に伴うfamily_post_imagesのdependentの修正

--- app/models/family_post.rb
+++ app/models/family_post.rb
@@ -2,6 +2,6 @@ class FamilyPost < ActiveRecord::Base
   belongs_to :family
   validates :content, presence: true, length: { maximum: 200 }
 
-  has_many :family_post_images
+  has_many :family_post_images, dependent: :destroy
   accepts_nested_attributes_for :family_post_images
 end
--- app/models/family_post_image.rb
+++ app/models/family_post_image.rb
@@ -1,4 +1,4 @@
 class FamilyPostImage < ActiveRecord::Base
-  belongs_to :family_post, :dependent => :destroy
+  belongs_to :family_post
   mount_uploader :image, FamilyPostImageUploader
 end
