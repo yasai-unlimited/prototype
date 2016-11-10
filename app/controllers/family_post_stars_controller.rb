dc20cb5a056cd1b280c9ba8fe9c2e6f8a74aaaa8
kei-tamiya <webcreatorhouse@gmail.com>
2016-11-10 23:30:21 +0900

    [modify] いいねボタン複数連打対応

--- app/controllers/family_post_stars_controller.rb
+++ app/controllers/family_post_stars_controller.rb
@@ -2,10 +2,12 @@ class FamilyPostStarsController < ApplicationController
   before_action :set_family_post
   def create
     current_family.family_post_star(@family_post)
+    @family_post_stars_count = @family_post.family_post_families.count
   end
 
   def destroy
     current_family.family_post_unstar(@family_post)
+    @family_post_stars_count = @family_post.family_post_families.count
   end
 
   private
