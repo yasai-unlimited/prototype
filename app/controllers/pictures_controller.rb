958e1bf651da6cb1e0c77601bba4de62ad1e97e6
Asami <doiasami1219@gmail.com>
2016-11-11 18:36:28 +0900

    [modify] edit new destroy

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -33,7 +33,7 @@ class PicturesController < ApplicationController
       if @picture.save
         # @album = @family.album.pictures.bulid
         # @album.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.', :flash => {:family_id => @family} }
+        format.html { redirect_to [@family, @picture], notice: 'Picture was successfully created.', :flash => {:family_id => @family} }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -47,7 +47,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to [@family, @picture], notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -61,7 +61,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to family_pictures_url, notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
