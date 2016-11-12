815ddffeb87327ed71b1b416e8457b6eaf3f3b41
Asami Doi <doiasami1219@gmail.com>
2016-11-12 20:22:11 +0900

    [modify] timeline

--- app/uploaders/family_icon_uploader.rb
+++ app/uploaders/family_icon_uploader.rb
@@ -36,7 +36,7 @@ class FamilyIconUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [480, 360]
+    process :resize_to_fill => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
