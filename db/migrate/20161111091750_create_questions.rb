062ca20b1b37627a77085de614358034b9b24d8c
Asami Doi <doiasami1219@gmail.com>
2016-11-13 01:28:53 +0900

    [modify] question form

--- db/migrate/20161111091750_create_questions.rb
+++ db/migrate/20161111091750_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :title, null: false
+      t.string :title
       t.text :content, null: false
       t.boolean :general_open
       t.boolean :friend_open
